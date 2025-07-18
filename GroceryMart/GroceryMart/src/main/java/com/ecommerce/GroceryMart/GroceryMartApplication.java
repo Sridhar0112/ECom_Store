02d5f03599ddb82468d05331c6fce9ce9117ae8c
SRIDHAR S <sridharsri7537@gmail.com>
2025-07-19 00:21:52 +0530

    Starting of the Project1

---
+++ GroceryMart/GroceryMart/src/main/java/com/ecommerce/GroceryMart/GroceryMartApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.GroceryMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GroceryMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryMartApplication.class, args);
+	}
+
+}
---
+++ GroceryMart/GroceryMart/src/main/java/com/ecommerce/GroceryMart/Main.java
@@ -0,0 +1,29 @@
+package com.ecommerce.GroceryMart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+
+@RequestMapping("/api")
+public class Main {
+
+    @GetMapping("/")
+    public  String getResult(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public  String login(){
+        return "Login";
+    }
+    @GetMapping("/register")
+    public  String register(){
+        return "Register";
+    }
+    @GetMapping("/products")
+    public  String products(){
+        return "Product";
+    }
+}
